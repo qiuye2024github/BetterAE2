2599067c178316219b411badb1ef7ef9cd116482
qiuye2024github <qiuye2024github@163.com>
2025-07-11 15:10:52 +0800

    init

---
+++ src/main/java/cn/qiuye/speedcard/SpeedCard.java
@@ -0,0 +1,14 @@
+package cn.qiuye.speedcard;
+
+import net.minecraftforge.fml.common.Mod;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SpeedCard.MOD_ID)
+public class SpeedCard {
+
+    public static final String MOD_ID = "speedcard";
+    public static final String NAME = "SpeedCard";
+    private static final Logger LOGGER = LogManager.getLogger(NAME);
+}
